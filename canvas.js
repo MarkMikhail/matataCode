a12ae25c286d1350e62efc66c21c47ca1dc66581
Mark Mikhail <mark_salib@outlook.com>
2020-04-27 09:07:20 +0300

    Add website files via upload
    Added the files successfully

---
+++ canvas.js
@@ -0,0 +1,139 @@
+const canvas = document.getElementById("myCanvas");
+const cn = canvas.getContext("2d");
+
+const center = [canvas.width/2, canvas.height/2];
+
+var order = "";
+
+var draw; // Interval variable
+var i = 0; // Delay variable
+
+const scale = canvas.width/8 - 10;
+const fps = 25;
+const orderSpeed = 2;
+
+function setup(){
+    // Styling and clearing the canvas
+    cn.strokeStyle = 'orange';
+    cn.lineWidth = 5;
+    cn.lineCap = "round";
+    cn.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Initialising position variables
+    targetX = currX = center[0];
+    targetR = currR = 0;
+    targetY = currY = center[1];
+
+    cn.beginPath();
+    cn.moveTo(currX, currY);
+
+    orderIndex = 0;
+    isOrderDone = true;
+}
+
+// Initialising position variables
+var targetX = currX = center[0];
+var targetY = currY = center[1];
+var targetR = currR = 0;
+
+var orderIndex = 0;
+var isOrderDone = true;
+
+var moveX, moveY, moveR;
+var errorX, errorY, errorR;
+
+function drawRobot(X, Y, theta){
+
+    cn.clearRect(0, 0, canvas.width, canvas.height);
+
+    var size = scale*0.4;
+    var radius = Math.sqrt(Math.pow(size/2,2)*2);
+    var triangle = new Path2D();
+    triangle.moveTo(X+radius*Math.sin(theta),Y-radius*Math.cos(theta));
+    triangle.lineTo(X+radius*Math.sin(theta+(2*Math.PI/3)),Y-radius*Math.cos(theta+(2*Math.PI/3)));
+    triangle.lineTo(X+radius*Math.sin(theta+(4*Math.PI/3)),Y-radius*Math.cos(theta+(4*Math.PI/3)));
+
+    var circle = new Path2D();
+    circle.arc(X, Y, radius, 0, 2 * Math.PI);
+    
+    cn.fillStyle = 'white';
+    cn.fill(circle);
+    cn.stroke(circle);
+    cn.fillStyle = 'orange';
+    cn.fill(triangle);
+}
+
+function draw(){
+  
+//   If last order is done get new order from order array
+    if (isOrderDone) {
+        
+    //   Set new target
+    //   Set order to not done
+    //   Set current order
+        
+        currOrder = order[orderIndex];
+        
+        switch (currOrder) {
+            case "F":
+                targetX += scale*Math.sin(currR);
+                targetY -= scale*Math.cos(currR);
+                break;
+            case "B":
+                targetX -= scale*Math.sin(currR);
+                targetY += scale*Math.cos(currR);
+                break;
+            case "R":
+                targetR += Math.PI/2;
+                break;
+            case "L":
+                targetR -= Math.PI/2;
+        }
+        
+        moveX = (targetX - currX)/(fps*orderSpeed);
+        moveY = (targetY - currY)/(fps*orderSpeed);
+        moveR = (targetR - currR)/(fps*orderSpeed);
+        
+        isOrderDone = false;
+        
+        console.log("Order Received");
+    }
+
+//   If order not done add a bit to current positions and directions (depending on current order)
+//   Check if target is met
+//   If met set order to done
+//   Add one to index
+    
+    errorX = Math.abs(targetX - currX);
+    errorY = Math.abs(targetY - currY);
+    errorR = Math.abs(targetR - currR);
+    
+    if(errorX < 1 && errorY < 1 && errorR < 0.01){
+        
+        i++;
+
+        if(i > Math.round(fps/2)){
+            isOrderDone = true;
+            orderIndex += 1;
+            console.log("Order Done");
+            i = 0;
+        }
+        
+
+        if(orderIndex === order.length){
+            clearInterval(draw);
+            console.log("STOPPED");
+        }
+
+    } else {
+        cn.lineTo(currX, currY);
+        drawRobot(currX, currY, currR);
+        cn.stroke();
+
+        currX += moveX;
+        currY += moveY;
+        currR += moveR;
+    }
+}
+
+// draw = setInterval(draw, 1000/fps);
---
+++ main.js
@@ -0,0 +1,139 @@
+const board = document.querySelector(".board");
+
+drawBlocks();
+
+const orderEmpty = document.querySelectorAll(".order");
+const stores = document.querySelectorAll(".storedOrder");
+const upArrow = document.querySelectorAll(".orderSub");
+
+const boardContainer = document.querySelector(".container")
+const blocks = document.querySelectorAll(".block")
+const storeAnim = document.querySelector(".storeContainer")
+const canvasAnim = document.querySelector("#myCanvas")
+
+let isMouseDown = false;
+let offset = [0, 0];
+let draggedElement = [];
+let targetOver;
+let orders;
+
+// Arrow listeners
+for(arrow of upArrow){
+    arrow.addEventListener("mousedown", dragStart);
+    arrow.addEventListener("touchstart", dragStart);
+}
+
+document.addEventListener("mousemove", dragProcess);
+document.addEventListener("touchmove", dragProcess);
+
+document.addEventListener("mouseup", dragEnd);
+document.addEventListener("touchend", dragEnd);
+
+document.querySelector("#play").addEventListener("click", function(){
+    orders = "";
+    for (empty of orderEmpty) {
+        if (empty.children.length) {
+            orders += empty.children[0].dataset.command;
+        }
+    }
+    order = orders;
+
+    boardContainer.classList.add("move");
+    storeAnim.classList.add("move");
+    canvasAnim.classList.add("move");
+    for (block of blocks){
+        block.classList.add("move");
+    }
+
+    setup()
+
+    draw = setInterval(draw, 1000/fps);
+})
+
+document.querySelector("#reset").addEventListener("click", function(){
+    boardContainer.classList.remove("move");
+    storeAnim.classList.remove("move");
+    canvasAnim.classList.remove("move");
+    for (block of blocks){
+        block.classList.remove("move");
+    }
+})
+
+function drawBlocks() {
+  for(var i = 0; i < 17; i++){
+    board.innerHTML += "<div class='block'><div class='order'></div><div class='number'></div></div>";
+  }
+}
+
+function dragStart(ev) {
+    ev.preventDefault();
+    try {
+        ev = ev.targetTouches[0];
+    } catch {} finally {
+        isMouseDown = true;
+        offset = [this.offsetLeft - ev.clientX, this.offsetTop - ev.clientY];
+        draggedElement.push(this);
+        // console.log(draggedElement);
+        draggedElement[0].style.pointerEvents = "none";
+    }
+}
+
+function dragProcess(ev){
+    ev.preventDefault();
+    try {
+        ev = ev.targetTouches[0];
+    } catch {} finally {
+        if (isMouseDown) {
+            // console.log('event triggered');
+            draggedElement[0].style.left = ev.clientX + offset[0] + 'px';
+            draggedElement[0].style.top = ev.clientY + offset[1] + 'px';
+            draggedElement[0].style.right = - parseInt(draggedElement[0].style.left) + 'px';
+            draggedElement[0].style.bottom = - parseInt(draggedElement[0].style.top) + 'px';
+
+            for (empty of orderEmpty) {
+                emptyRect = empty.getBoundingClientRect();
+                if(ev.clientX > emptyRect.left & ev.clientX < emptyRect.right & ev.clientY > emptyRect.top & ev.clientY < emptyRect.bottom){
+                    if (empty.children.length === 0){
+                        empty.classList.add("hovered");
+                        targetOver = empty;
+                    }
+                } else {
+                    empty.classList.remove("hovered");
+                }
+            }
+
+            for (store of stores) {
+                storeRect = store.getBoundingClientRect();
+                if(ev.clientX > storeRect.left & ev.clientX < storeRect.right & ev.clientY > storeRect.top & ev.clientY < storeRect.bottom){
+                    if(store.dataset.store === draggedElement[0].dataset.command){
+                        store.classList.add("hovered");
+                        targetOver = store;
+                    }
+                } else {
+                    store.classList.remove("hovered");
+                }
+            }
+        }
+    }
+}
+
+function dragEnd(ev){
+    isMouseDown = false;
+    // console.log("Mouse Up");
+    if(draggedElement[0]){
+        if(targetOver){
+            targetOver.append(draggedElement[0]);
+            targetOver.classList.remove("hovered");
+            targetOver = undefined;
+        }
+        draggedElement[0].style.left = 0 + 'px';
+        draggedElement[0].style.top = 0 + 'px';
+        draggedElement[0].style.right = - parseInt(draggedElement[0].style.left) + 'px';
+        draggedElement[0].style.bottom = - parseInt(draggedElement[0].style.top) + 'px';
+        
+        draggedElement[0].style.pointerEvents = "auto";
+        draggedElement.pop();
+    }
+
+    // console.log(draggedElement);
+}
